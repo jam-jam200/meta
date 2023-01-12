919a8046e7724bbbadca25cdff9bf67115edfb9a
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-12 12:00:22 +0100

    set up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{html,js,jsx}',
-    './components/**/*.{html,js,jsx}',
-    './sections/**/*.{html,js,jsx}',
-    './styles/**/*.{js,jsx}',
+    './index.html',
+    './src/**/*.{js,jsx}',
   ],
   mode: 'jit',
   theme: {
