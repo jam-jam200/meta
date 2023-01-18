837ace4c80846d56a66e5fd6a6987ec2c1f3d963
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-18 15:37:25 +0100

    ..

--- src/components/ExploreCard.jsx
+++ src/components/ExploreCard.jsx
@@ -9,7 +9,26 @@ const ExploreCard = ({ id, title, imgUrl, index, active, handleClick }) => (
       active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
   >
-    Explore Card
+    <img
+      src={imgUrl}
+      alt={title}
+      className="w-full h-full object-cover rounded-[24px]"
+    />
+    {active !== id ? (
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+        {title}
+      </h3>
+    ) : (
+      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+        <div>
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-1/2 h-1/2 object-contain"
+          />
+        </div>
+      </div>
+    )}
   </motion.div>
 );
 
