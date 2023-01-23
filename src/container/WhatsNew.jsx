8a352a00a71c330cd45ea31d840995cdea2acb2f
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-23 13:29:06 +0100

    getstarted done

--- src/container/WhatsNew.jsx
+++ src/container/WhatsNew.jsx
@@ -1,9 +1,9 @@
-'use client';
+import { motion } from "framer-motion";
+import { NewFeatures, TitleText, TypingText } from "../components";
+import styles from "../styles";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { newFeatures } from "../constants";
 
-const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
-);
+const WhatsNew = () => <section>What's new section</section>;
 
 export default WhatsNew;
