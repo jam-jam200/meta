58c1e569b6997f12a168286cf030dea948149790
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-25 12:29:52 +0100

    working on insights

--- src/container/Insights.jsx
+++ src/container/Insights.jsx
@@ -4,6 +4,20 @@ import styles from "../styles";
 import { staggerContainer } from "../utils/motion";
 import { insights } from "../constants";
 
-const Insights = () => <section>Insights section</section>;
+const Insights = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insights" textStyles="text-center" />
+      <TitleText title="Insight about metaverse" textStyles="text-center" />
+      
+    </motion.div>
+  </section>
+);
 
 export default Insights;
