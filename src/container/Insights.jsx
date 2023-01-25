0ffe79997adb7d33290f5a15c16b59afef7ca729
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-25 12:24:35 +0100

    working on insights

--- src/container/Insights.jsx
+++ src/container/Insights.jsx
@@ -1,9 +1,9 @@
-'use client';
+import { motion } from "framer-motion";
+import { InsightCard, TitleText, TypingText } from "../components";
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { insights } from "../constants";
 
-const Insights = () => (
-  <section>
-    Insights section
-  </section>
-);
+const Insights = () => <section>Insights section</section>;
 
 export default Insights;
