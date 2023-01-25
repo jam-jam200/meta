816be705631b35b743764d7cd8780f72f030f6bf
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-25 13:22:08 +0100

    working on feedback section

--- src/container/Feedback.jsx
+++ src/container/Feedback.jsx
@@ -1,8 +1,33 @@
-'use client';
+import { motion } from "framer-motion";
+import { InsightCard, TitleText, TypingText } from "../components";
+import styles from "../styles";
+import { staggerContainer, fadeIn, zoomIn } from "../utils/motion";
+import { insights } from "../constants";
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[32px] text-white">
+            Samantha
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Meterversus
+          </p>
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
