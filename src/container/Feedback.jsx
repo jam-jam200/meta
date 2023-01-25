8d57b993763049e0f54b9da9c820608922716692
jam-jam200 <jessieoyaks4j@gmail.com>
2023-01-25 14:28:05 +0100

    working on feedback section

--- src/container/Feedback.jsx
+++ src/container/Feedback.jsx
@@ -26,6 +26,21 @@ const Feedback = () => (
             Founder | Meterversus
           </p>
         </div>
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+          “With the development of today's technology, metaverse is very useful
+          for today's work, or can be called web 3.0. by using metaverse you can
+          use it as anything”
+        </p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/planet-09.png"
+          alt="planet-09"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
       </motion.div>
     </motion.div>
   </section>
